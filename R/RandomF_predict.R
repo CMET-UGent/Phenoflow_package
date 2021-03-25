e8d096bc5f86718cafb5914017a067fedbfc707e
fpkerckh <frederiekmaarten.kerckhof@ugent.be>
2021-03-25 23:57:37 +0100

    updated flowAI params for RandomF_predict

--- R/RandomF_predict.R
+++ R/RandomF_predict.R
@@ -61,11 +61,11 @@ RandomF_predict <- function(x, new_data, cleanFCS = FALSE,
                               folder_results = "QC_flowAI",
                               fcs_highQ = "HighQ",
                               output = 1,
-                              ChFM = paste0(param_f[!param_f %in% 
+                              ChExcludeFM = paste0(param_f[!param_f %in% 
                                                       c("FSC","SSC")],
                                             "-", add_measuredparam),
                               timeCh=TimeChannel,
-                              ChRemoveFS = filter_param,
+                              ChExcludeFS = filter_param,
                               second_fractionFR = timesplit
     )
     
